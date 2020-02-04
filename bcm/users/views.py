4ffda5f5c9ea46fed6a0e24c6ecc6990891db98f
fsalehnia <fatemeh.s@renewablion.com>
2020-02-04 22:46:36 +0330

    Merge branch 'dev' of C:\Users\Asus\PycharmProjects\v2_bcm\BCM with conflicts.

--- bcm/users/views.py
+++ bcm/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.utils.translation import gettext
+from django.utils import translation
 from users.forms import SignUpForm
 
 
@@ -14,21 +15,40 @@ def register(request):
         form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
 
-# def user_login(request):
-#     next_path = None
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(
-#                 request,
-#                 username=cd['username'],
-#                 password=cd['password']
-#             )
-#             if user and user.is_active
-#     else:
-#         form = LoginForm()
-#         if request.method == 'GET':
-#             next_path = request.GET.get('next')
-#
-#     return render(request, 'users/login.html', {'form': form, 'next': next_path})
+############Dashboard Base section #######################
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard_base.html')
+
+############ Residence section #######################
+
+def ResidenceManagePage(request):
+    return render(request, 'dashboard/residence/residence_managment.html')
+
+def NewResidencePage(request):
+    return render(request, 'dashboard/residence/New_residence.html')
+
+############ Block section #######################
+
+def BlockManagePage(request):
+    return render(request, 'dashboard/Blocks/block_managment.html')
+
+def NewBlockPage(request):
+    return render(request, 'dashboard/Blocks/New_block.html')
+
+############ Unit section #######################
+
+def UnitManagePage(request):
+    return render(request, 'dashboard/Units/unit_managment.html')
+
+def NewUnitPage(request):
+    return render(request, 'dashboard/Units/New_unit.html')
+
+############ Facility section #######################
+
+def FacilityManagePage(request):
+    return render(request, 'dashboard/Facilities/Facility_managment.html')
+
+def NewFacilityPage(request):
+    return render(request, 'dashboard/Facilities/New_Facility.html')
+
