f0dfe0352a4347cdb6b98f52ef66a34832722d1c
fsalehnia <fatemeh.s@renewablion.com>
2020-02-05 00:49:45 +0330

    Add Dashboard Templates - v1

--- bcm/building/views.py
+++ bcm/building/views.py
@@ -1,1 +1,41 @@
-# Create your views here.
+from django.shortcuts import render
+from django.utils.translation import gettext
+from django.utils import translation
+
+############Dashboard Base section #######################
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard_base.html')
+
+############ Residence section #######################
+
+def ResidenceManagePage(request):
+    return render(request, 'dashboard/residence/residence_managment.html')
+
+def NewResidencePage(request):
+    return render(request, 'dashboard/residence/New_residence.html')
+
+############ Block section #######################
+
+def BlockManagePage(request):
+    return render(request, 'dashboard/Blocks/block_managment.html')
+
+def NewBlockPage(request):
+    return render(request, 'dashboard/Blocks/New_block.html')
+
+############ Unit section #######################
+
+def UnitManagePage(request):
+    return render(request, 'dashboard/Units/unit_managment.html')
+
+def NewUnitPage(request):
+    return render(request, 'dashboard/Units/New_unit.html')
+
+############ Facility section #######################
+
+def FacilityManagePage(request):
+    return render(request, 'dashboard/Facilities/Facility_managment.html')
+
+def NewFacilityPage(request):
+    return render(request, 'dashboard/Facilities/New_Facility.html')
+
--- bcm/users/urls.py
+++ bcm/users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from users.views import register,dashboard,ResidenceManagePage,NewResidencePage,NewBlockPage,BlockManagePage,UnitManagePage,NewUnitPage,FacilityManagePage,NewFacilityPage
+from users.views import register
 
 app_name = 'users'
 
@@ -10,17 +10,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', auth_views.LoginView.as_view()),
-    #  Dashboard Paths =======================
-    path('dashboard/', dashboard, name='dashboard'),
-    path('dashboard/residence', ResidenceManagePage, name='ResidenceManagePage'),
-    path('dashboard/residence/new', NewResidencePage, name='NewResidencePage'),
-    ######## Block Path ############
-    path('dashboard/block', BlockManagePage, name='BlockManagePage'),
-    path('dashboard/block/new', NewBlockPage, name='NewBlockPage'),
-    ######## Unit Path ############
-    path('dashboard/unit', UnitManagePage, name='UnitManagePage'),
-    path('dashboard/unit/new', NewUnitPage, name='NewUnitPage'),
-    ######## Unit Path ############
-    path('dashboard/facility', FacilityManagePage, name='FacilityManagePage'),
-    path('dashboard/facility/new', NewFacilityPage, name='NewFacilityPage'),
+
 ]
