662d6d3b3468d79590084be4520a04488d17b27f
fsalehnia <fatemeh.s@renewablion.com>
2020-02-10 10:58:30 +0330

    Add new and delete to Templates

--- bcm/building/views.py
+++ bcm/building/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils.translation import gettext
 from django.utils import translation
+from django.http import JsonResponse
 
 ############Dashboard Base section #######################
 
@@ -9,33 +10,66 @@ def dashboard(request):
 
 ############ Residence section #######################
 
-def ResidenceManagePage(request):
+def residence_manage_page(request):
     return render(request, 'dashboard_pages/residence/residence_managment.html')
 
-def NewResidencePage(request):
-    return render(request, 'dashboard_pages/residence/New_residence.html')
+def new_residence_page(request):
+    return render(request, 'dashboard_pages/residence/new_residence.html')
 
+def delete_residence_page(request):
+    def get(self, request):
+        id1 = request.GET.get('residence_id', None)
+        # residence.objects.get(id=id1).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)
 ############ Block section #######################
 
-def BlockManagePage(request):
-    return render(request, 'dashboard_pages/Blocks/block_managment.html')
+def block_manage_page(request):
+    return render(request, 'dashboard_pages/blocks/block_managment.html')
 
-def NewBlockPage(request):
-    return render(request, 'dashboard_pages/Blocks/New_block.html')
+def new_block_page(request):
+    return render(request, 'dashboard_pages/blocks/new_block.html')
+
+def delete_block_page(request):
+    def get(self, request):
+        id1 = request.GET.get('block_id', None)
+        # residence.objects.get(id=id1).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)
 
 ############ Unit section #######################
 
-def UnitManagePage(request):
-    return render(request, 'dashboard_pages/Units/unit_managment.html')
+def unit_manage_page(request):
+    return render(request, 'dashboard_pages/units/unit_managment.html')
 
-def NewUnitPage(request):
-    return render(request, 'dashboard_pages/Units/New_unit.html')
+def new_unit_page(request):
+    return render(request, 'dashboard_pages/units/new_unit.html')
+def delete_unit_page(request):
+    def get(self, request):
+        id1 = request.GET.get('unit_id', None)
+        # residence.objects.get(id=id1).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)
 
 ############ Facility section #######################
 
-def FacilityManagePage(request):
-    return render(request, 'dashboard_pages/Facilities/Facility_managment.html')
+def facility_manage_page(request):
+    return render(request, 'dashboard_pages/facilities/facility_managment.html')
 
-def NewFacilityPage(request):
-    return render(request, 'dashboard_pages/Facilities/New_Facility.html')
+def new_facility_page(request):
+    return render(request, 'dashboard_pages/facilities/new_facility.html')
+def delete_facility_page(request):
+    def get(self, request):
+        id1 = request.GET.get('facility_id', None)
+        # residence.objects.get(id=id1).delete()
+        data = {
+            'deleted': True
+        }
+        return JsonResponse(data)
 
