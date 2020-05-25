ba0fde3b59d46f8fd870c21d61b48b2ec95e7095
bcmgithubco <pro.amiri.123@gmail.com>
2020-05-25 10:46:49 +0430

    fix migration bug

--- bcm/core/services.py
+++ bcm/core/services.py
@@ -1,23 +1,25 @@
 from django.contrib.contenttypes.models import ContentType
 
+try:
+    residence = ContentType.objects.get(model='residence')
+    block = ContentType.objects.get(model='block')
+    unit = ContentType.objects.get(model='unit')
 
-residence = ContentType.objects.get(model='residence')
-block = ContentType.objects.get(model='block')
-unit = ContentType.objects.get(model='unit')
+    accounting_target_representation = {str(residence.id): 'residence',
+                                        'residence': residence,
+                                        str(block.id): 'block',
+                                        'block': block,
+                                        str(unit.id): 'unit',
+                                        'unit': unit}
 
-accounting_target_representation = {str(residence.id): 'residence',
-                                    'residence': residence,
-                                    str(block.id): 'block',
-                                    'block': block,
-                                    str(unit.id): 'unit',
-                                    'unit': unit}
-
-accounting_target_internal = {str(residence.id): residence,
-                              'residence': residence,
-                              str(block.id): block,
-                              'block': block,
-                              str(unit.id): unit,
-                              'unit': unit}
+    accounting_target_internal = {str(residence.id): residence,
+                                  'residence': residence,
+                                  str(block.id): block,
+                                  'block': block,
+                                  str(unit.id): unit,
+                                  'unit': unit}
+except:
+    pass
 
 
 def content_type_converter(key, mode, id=True):
