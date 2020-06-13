485b1e0679e0aae4dd1def5ed4b624ca9c967fb7
fsalehnia <fatemeh.s@renewablion.com>
2020-06-13 17:02:28 +0430

    add Landing Page - v1

--- bcm/users/urls.py
+++ bcm/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from users.views import register
+from users.views import register,landing_page
 
 app_name = 'users'
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', auth_views.LoginView.as_view()),
+    path('', landing_page, name='landing_page'),
+
+    # path('', auth_views.LoginView.as_view()),
 
 ]
